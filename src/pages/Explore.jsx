cd4c763b6226cdb189687b502e5d93fa0bac243c
compean <javier.compeanr@gmail.com>
2022-02-16 20:00:23 -0600

    starting with listing implementation

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import rentCategoryImage from "../assets/jpg/rentCategoryImage.jpg";
+import sellCategoryImage from "../assets/jpg/sellCategoryImage.jpg";
 
 function Explore() {
-  return <h1>Explore</h1>;
+  return (
+    <div className="explore">
+      <header>
+        <p className="pageHeader"></p>
+      </header>
+
+      <main>
+        {/* Slider */}
+
+        <p className="exploreCategoryHeading">Categories</p>
+        <div className="exploreCategories">
+          <Link to="/category/rent">
+            <img
+              src={rentCategoryImage}
+              alt="rent"
+              className="exploreCategoryImg"
+            />
+            <p className="exploreCategoryName">Place for rent</p>
+          </Link>
+
+          <Link to="/category/sale">
+            <img
+              src={sellCategoryImage}
+              alt="rent"
+              className="exploreCategoryImg"
+            />
+            <p className="exploreCategoryName">Place for sale</p>
+          </Link>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default Explore;
