1a8e9349d31cf11363d82a27f67e4767bf9c5f63
compean <javier.compean@gmail.com>
2022-02-28 13:40:11 -0600

    createlisting page finished

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -88,13 +88,24 @@ function CreateListing() {
 
     if (geolocationEnabled) {
       const response = await fetch(
-        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyBzXKfykZVLrMHsL8OjW3cM_PKu1JUfRrI`
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`
       );
 
       const data = await response.json();
 
-      geolocation.lat = data.results[0].geometry.location.lat ?? 0;
-      geolocation.lng = data.results[0].geometry.location.lng ?? 0;
+      geolocation.lat = data.results[0]?.geometry.location.lat ?? 0;
+      geolocation.lng = data.results[0]?.geometry.location.lng ?? 0;
+
+      location =
+        data.status === "ZERO_RESULTS"
+          ? undefined
+          : data.results[0]?.formatted_address;
+
+      if (location === undefined || location.includes("undefined")) {
+        setLoading(false);
+        toast.error("Please enter a correct address");
+        return;
+      }
     } else {
       geolocation.lat = latitude;
       geolocation.lng = longitude;
