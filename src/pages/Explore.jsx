b0595ba471795dd232f904edd079fb6564d1cf26
compean <javier.compeanr@gmail.com>
2022-03-01 20:11:17 -0600

    profile page listing in progress

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import { Link } from "react-router-dom";
 import rentCategoryImage from "../assets/jpg/rentCategoryImage.jpg";
 import sellCategoryImage from "../assets/jpg/sellCategoryImage.jpg";
+import Slider from "../components/Slider";
 
 function Explore() {
   return (
     <div className="explore">
       <header>
-        <p className="pageHeader"></p>
+        <p className="pageHeader">Explore</p>
       </header>
 
       <main>
         {/* Slider */}
+        <Slider />
 
         <p className="exploreCategoryHeading">Categories</p>
         <div className="exploreCategories">
